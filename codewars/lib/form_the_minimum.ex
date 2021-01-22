5ed299f9a9a6701b270ef96ae514ef3f262bf20a
Dawid Dęby <ddeby@grupazpr.pl>
2021-01-22 11:50:31 +0100

    Form the minimum number from list of digits. Simple solution based on Enum->uniq->sort->join->integer

---
+++ codewars/lib/form_the_minimum.ex
@@ -0,0 +1,9 @@
+defmodule FormTheMinimum do
+  def minValue(numbers) do
+    # my solution
+    numbers |> Enum.uniq() |> Enum.sort() |> Enum.join() |> String.to_integer()
+
+    # solution on CodeWars (undigits)
+    numbers |> Enum.uniq() |> Enum.sort() |> Integer.undigits()
+  end
+end
