074da7108c0edb2abd4e1e1ea4093b05bb9c8573
Dawid Dęby <dawid.deby@gmail.com>
2021-01-26 10:20:07 +0100

    mix format

--- codewars/lib/highest_and_lowest.ex
+++ codewars/lib/highest_and_lowest.ex
@@ -1,7 +1,16 @@
 defmodule HighestAndLowest do
   def high_and_low(s) do
-    nums = s |> String.split(" ") |> Enum.map(fn x -> String.to_integer(x) end)
-    "#{Enum.max(nums)} #{Enum.min(nums)}"
+    # My solution
+    # nums = s |> String.split(" ") |> Enum.map(fn x -> String.to_integer(x) end)
+    # "#{Enum.max(nums)} #{Enum.min(nums)}"
+    # TIL: Enum.min_max
+    # Best solution on Codewars
+    {min, max} =
+      s
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer(&1))
+      |> Enum.min_max()
+
+    "#{max} #{min}"
   end
 end
-
--- codewars/lib/rock_paper_scissors.ex
+++ codewars/lib/rock_paper_scissors.ex
@@ -3,5 +3,5 @@ defmodule RockPaperScissors do
   def rps(p1, p2) when p1 == "paper" and p2 == "rock", do: "Player 1 won!"
   def rps(p1, p2) when p1 == "rock" and p2 == "scissors", do: "Player 1 won!"
   def rps(p1, p2) when p1 == "scissors" and p2 == "paper", do: "Player 1 won!"
-  def rps(p1, p2), do: "Player 2 won!"
+  def rps(_, _), do: "Player 2 won!"
 end
