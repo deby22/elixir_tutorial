a3546ef2bc6147c3a9987c0c75e92ce2430d547a
Dawid Dęby <dawid.deby@gmail.com>
2021-03-16 13:15:58 +0100

    add server and client part of GenServer

--- programming_elixir/stack/lib/stack/server.ex
+++ programming_elixir/stack/lib/stack/server.ex
@@ -1,8 +1,23 @@
 defmodule Stack.Server do
   use GenServer
 
-  def init(init_state) do
-    {:ok, init_state}
+  # Client
+  def start_link(init_state) do
+    GenServer.start_link(__MODULE__, init_state, name: __MODULE__)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(value) do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
+  # Server
+  def terminate(reason, stack) do
+    IO.puts("Reason: #{reason} \t\tStack#{stack}")
+    :normal
   end
 
   def handle_call(:pop, _from, [element | stack]) do
